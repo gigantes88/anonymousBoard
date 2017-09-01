6fd10b32846679458589f2dbc88c89ba927b2947
gigantes88 <gigantes88@gmail.com>
2017-09-01 20:42:04 +0900

    리스트 가져오기, 글쓰기, 글 상세보기 성공!

--- server.js
+++ server.js
@@ -1,18 +1,49 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-const data = [
-  {id: '1', title: '익명게시판이지롱', writer: '아무개'}
-];
+const bodyParserMiddleware = bodyParser.urlencoded({ extended: false });  // body-parser 미들웨어 설정
+
+const data = [];
 
 app.set('view engine', 'ejs');  // ejs 설정
 app.use('/static', express.static('./public')); // static 설정
 
+// index 화면 render
 app.get('/', (req, res) => {
   res.render('index', {data});
 });
 
-// app.get('/:id', (req, res) => {
+// view 화면 render
+app.get('/view', (req, res) => {
+  res.render('view', {data});
+});
+
+// view 화면 가져오기
+app.get('/view/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const matched = [...data].find(item => item.id === id);
+  
+  if (matched) {
+    res.render('view.ejs', {matched});
+  } else {
+    res.status(404);
+    res.send('404 떠서 기분이 안좋다');
+  }
+
+});
+
+// 글쓰기
+app.post('/view', bodyParserMiddleware, (req, res) => {
+  let id = data.length + 1;
+  const title = req.body.title;
+  const contents = req.body.contents;
+  const writer = req.body.writer;
+  
+  data.push({id, title, contents, writer });
+  res.redirect('/');
+  console.log(data);
+  
 });
 
 app.listen(3000, () => {
