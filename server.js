f6eb218d524cb4a3dbbdf50e337e56f2447a97f3
gigantes88 <gigantes88@gmail.com>
2017-09-01 18:52:18 +0900

    데이터 가져오기 성공

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
 
+const data = [
+  {id: '1', title: '익명게시판이지롱', writer: '아무개'}
+];
+
 app.set('view engine', 'ejs');  // ejs 설정
 app.use('/static', express.static('./public')); // static 설정
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {data});
+});
+
+// app.get('/:id', (req, res) => {
 });
 
 app.listen(3000, () => {
   console.log('listening to 3000 port');
-})
+});
